05f4f395830e4e5efe52f91dc91bb25e102a382a
kevinlu323 <kevinlu323@gmail.com>
2015-07-03 16:35:58 -0500

    Initial commit

---
+++ src/com/linkui/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.linkui;
+
+/**
+ * Created by linkui on 7/3/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, This is the 1st JAVA app created by Idea IDE!");
+    }
+}
